1c727982a31f34bfa0e633812262b2db6a346f1a
Akshat Meena <aksmeena@publicisgroupe.net>
2025-02-03 18:40:04 +0530

    use input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+import { getInput } from "@actions/core";
+
+const name = getInput("name");
+
+greet(name);
+
+function greet(name: string){
+	console.log(`Hello ${name}`);
+}
