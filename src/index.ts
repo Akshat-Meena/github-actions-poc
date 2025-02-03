eff986f3f44ae4c3fffad1a55fe2027c903c45ad
Akshat Meena <aksmeena@publicisgroupe.net>
2025-02-03 18:55:38 +0530

    add secret

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { getInput } from "@actions/core";
 
 const name = getInput("name");
+const testSecret = getInput("test_secret");
 
 greet(name);
+printSecret(testSecret);
 
 function greet(name: string){
 	console.log(`Hello ${name}`);
+}
+
+function printSecret(secret: string){
+	console.log(`Secret: ${secret}`);
 }
